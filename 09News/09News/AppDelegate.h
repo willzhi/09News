d218068c0de8598ab1f3c2e0a923ecb90f45f3ed
gzxzmac <gzxzmac@iosjsj.local>
2016-02-26 09:57:22 +0800

    新建项目

---
+++ 09News/09News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  09News
+//
+//  Created by gzxzmac on 16/2/26.
+//  Copyright © 2016年 gzxzmac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 09News/09News/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  09News
+//
+//  Created by gzxzmac on 16/2/26.
+//  Copyright © 2016年 gzxzmac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
